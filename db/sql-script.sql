e00ecdc435b70b070cccf129786d8622bd75442c
iamhuynq <nguyenquanghuy0511@gmail.com>
2020-06-19 11:33:35 +0700

    Set up mysql and connecting with nodejs

---
+++ db/sql-script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE registers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL
+);
